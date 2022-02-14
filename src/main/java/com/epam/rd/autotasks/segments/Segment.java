8c9068d86d6ecada40b623f6694c89610b80a606
Andrei Pronsky <andrey.pronsky@gmail.com>
2022-02-14 13:23:19 +0300

    Completed

--- src/main/java/com/epam/rd/autotasks/segments/Segment.java
+++ src/main/java/com/epam/rd/autotasks/segments/Segment.java
@@ -5,21 +5,42 @@ import static java.lang.Math.sqrt;
 import static java.lang.StrictMath.pow;
 
 class Segment {
+    private final double endX;
+    private final double startX;
+    private final double endY;
+    private final double startY;
 
     public Segment(Point start, Point end) {
-
+        this.startX = start.getX();
+        this.endX = end.getX();
+        this.startY = start.getY();
+        this.endY = end.getY();
+        if (startX == endX && startY == endY) {
+            throw new IllegalArgumentException();
+        }
     }
 
     double length() {
-
+        double length = Math.sqrt(Math.pow((endX - startX), 2) + Math.pow((endY - startY), 2));
+        if (length != 0) {
+            return length;
+        }
+        else{
+            throw new IllegalArgumentException();
+        }
     }
 
-    Point middle() {
-
+     Point middle() {
+         return new Point((startX + endX)/2, (startY + endY)/2);
     }
 
     Point intersection(Segment another) {
-
+        double d = ((this.startX-this.endX) * (another.startY-another.endY) - (this.startY-this.endY) * (another.startX-another.endX));
+        double intersX = ((this.startX*this.endY-this.startY*this.endX) * (another.startX-another.endX) - (this.startX-this.endX) * (another.startX*another.endY-another.startY*another.endX)) / d;
+        double intersY = ((this.startX*this.endY-this.startY*this.endX) * (another.startY-another.endY) - (this.startY-this.endY) * (another.startX*another.endY-another.startY*another.endX)) / d;
+        if((Math.min(this.startX, this.endX)) <= intersX && intersX <= (Math.max(this.startX, this.endX)) && (Math.min(another.startX,another.endX)) <= intersX && intersX<= (Math.max(another.startX,another.endX))) {
+            return new Point(intersX, intersY);
+        }
+        else{ return null;}
     }
-
 }
--- src/test/java/com/epam/rd/autotasks/segments/Main.java
+++ src/test/java/com/epam/rd/autotasks/segments/Main.java
@@ -4,17 +4,20 @@ public class Main {
     public static void main(String[] args) {
 
         {
-            double length = new Segment(new Point(0, 0), new Point(3, 4)).length();
-            System.out.println(length);
+            //double length = new Segment(new Point(0, 0), new Point(3, 4)).length();
+            //System.out.println(length);
+
         }
 
         {
-            Segment first = new Segment(new Point(0, 0), new Point(4, 4));
-            Segment second = new Segment(new Point(2, 0), new Point(0, 2));
-            Point intersection = first.intersection(second);
-
-            System.out.println(intersection.getX());
-            System.out.println(intersection.getY());
+            Segment first = new Segment(new Point(0, 0), new Point(1, 1));
+            //Segment second = new Segment(new Point(2, 0), new Point(0, 2));
+            //Point intersection = first.intersection(second);
+            Point middle = first.middle();
+            //System.out.println(intersection.getX());
+            //System.out.println(intersection.getY());
+            System.out.println(middle.getX());
+            System.out.println(middle.getY());
         }
 
         {
